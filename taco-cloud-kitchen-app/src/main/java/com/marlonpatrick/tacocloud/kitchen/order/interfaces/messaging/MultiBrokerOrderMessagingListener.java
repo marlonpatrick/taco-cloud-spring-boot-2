783f65ac9693b5bd13206821d5e55ae4f28f4ab5
Marlon Patrick <marlon.patrick@alclog.com.br>
2019-04-28 19:00:46 -0300

    spring rabbit listener (push)

--- taco-cloud-kitchen-app/src/main/java/com/marlonpatrick/tacocloud/kitchen/order/interfaces/messaging/MultiBrokerOrderMessagingListener.java
+++ taco-cloud-kitchen-app/src/main/java/com/marlonpatrick/tacocloud/kitchen/order/interfaces/messaging/MultiBrokerOrderMessagingListener.java
@@ -1,14 +1,16 @@
-package com.marlonpatrick.tacocloud.kitchen.order.interfaces.messaging.jms;
+package com.marlonpatrick.tacocloud.kitchen.order.interfaces.messaging;
 
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 import com.marlonpatrick.tacocloud.kitchen.order.Order;
 
 @Component
-public class JMSOrderMessagingListener {
+public class MultiBrokerOrderMessagingListener {
 
 	@JmsListener(destination="tacocloud.order.queue")
+	@RabbitListener(queues="tacocloud.order.queue")
 	public void receiveOrder(Order order) {
 		System.out.println("OrderListener...");
 		System.out.println(order);
